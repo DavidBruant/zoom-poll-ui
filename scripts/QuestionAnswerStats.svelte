aa8c86e3f11da674e18a82de0dbb35c71fdea70c
David Bruant <davidbruant@protonmail.com>
2020-09-19 20:10:31 +0200

    Change answers style

--- scripts/QuestionAnswerStats.svelte
+++ scripts/QuestionAnswerStats.svelte
@@ -5,24 +5,40 @@
     
     $: sortedAnswerStats =[...answerStats]
         .sort(([_, count1], [__, count2]) => count2 - count1)
-        .map(([answer, count]) => ({answer, items: Array(count).fill('🧘')}))
+        .map(([answer, count]) => ({answer, count}))
 </script>
 
 <section class="question-answer-stats">
     <h1>{question} ({answersCount} réponses)</h1>
     <ul>
-        {#each sortedAnswerStats as {answer, items}}
+        {#each sortedAnswerStats as {answer, count}}
         <li>
-            {#each items as i}{i}{/each}
-            <span>{answer}</span>
+            <span class="count">{count}/{answersCount}</span>
+            <span class="answer">{answer}</span>
         </li>
         {/each}
     </ul>
 </section>
 
 <style>
+    .question-answer-stats{
+        margin-bottom: 1em;
+    }
+
     .question-answer-stats h1{
         font-size: 1.2rem;
+    }
+
+    .question-answer-stats li{
+        display: flex;
+        flex-direction: row;
+    }
+
+    .question-answer-stats li .count{
+        display: inline-block;
+        width: 4em;
+        font-weight: bold;
+        text-align: center;
     }
     
 </style>
